273fab474ca4f677268d7980a6a739e7cf56651b
Indrek Mandre <indrek@mare.ee>
2025-02-19 22:31:52 +0200

    inline _frameset0

--- llvm/lib/Target/Z80/Z80FrameLowering.cpp
+++ llvm/lib/Target/Z80/Z80FrameLowering.cpp
@@ -236,11 +236,22 @@ void Z80FrameLowering::emitPrologue(MachineFunction &MF,
               .addReg(Z80::AF, RegState::ImplicitDefine) // we clobber af inside _frameset
               .setMIFlag(MachineInstr::FrameSetup);
         }
-      } else
-        BuildMI(MBB, MBBI, DL, TII.get(Is24Bit ? Z80::CALL24 : Z80::CALL16))
-            .addExternalSymbol("_frameset0")
-            .addRegMask(TRI->getNoPreservedMask())
-            .setMIFlag(MachineInstr::FrameSetup);
+      } else {
+        if (!Is24Bit) {
+          BuildMI(MBB, MBBI, DL, TII.get(Z80::PUSH16r), Z80::IX)
+              .setMIFlag(MachineInstr::FrameSetup);
+          BuildMI(MBB, MBBI, DL, TII.get(Z80::LD16ri), Z80::IX)
+              .addImm(0)
+              .setMIFlag(MachineInstr::FrameSetup);
+          BuildMI(MBB, MBBI, DL, TII.get(Z80::ADD16as), Z80::IX)
+              .setMIFlag(MachineInstr::FrameSetup);
+        } else {
+          BuildMI(MBB, MBBI, DL, TII.get(Is24Bit ? Z80::CALL24 : Z80::CALL16))
+              .addExternalSymbol("_frameset0")
+              .addRegMask(TRI->getNoPreservedMask())
+              .setMIFlag(MachineInstr::FrameSetup);
+        }
+      }
       if (MF.needsFrameMoves()) {
         BuildMI(MBB, MBBI, DL, TII.get(TargetOpcode::CFI_INSTRUCTION))
             .addCFIIndex(MF.addFrameInst(MCCFIInstruction::cfiDefCfa(
