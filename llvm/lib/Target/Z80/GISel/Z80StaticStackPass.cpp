365a0978d3fe67b578388b7035fe54f67ff2fd7f
Indrek Mandre <indrek@mare.ee>
2025-02-23 12:55:59 +0200

    static stack support for inc8o/dec8o

--- llvm/lib/Target/Z80/GISel/Z80StaticStackPass.cpp
+++ llvm/lib/Target/Z80/GISel/Z80StaticStackPass.cpp
@@ -287,6 +287,23 @@ bool Z80StaticStackPass::runOnMachineFunction(MachineFunction &MF)
         MI.eraseFromParent();
         continue;
       }
+
+      if ((MI.getOpcode() == Z80::INC8o || MI.getOpcode() == Z80::DEC8o) && MI.getOperand(0).isFI() && MI.getOperand(1).isImm()) {
+        int frame_index = MI.getOperand(0).getIndex();
+        int64_t offset = MI.getOperand(1).getImm();
+        auto fit = addresses.find(frame_index);
+        if (fit == addresses.end())
+          continue;
+        size_t address = fit->second;
+        LLVM_DEBUG(dbgs() << "Z80StaticStackPass: TargetOpcode::INC8o/DEC8o: " << frame_index << ": staticStack+" << address << "+" << offset << "\n");
+        assert(canTransformEverything);
+        need_IX = true;
+        BuildMI(MBB, MI, MI.getDebugLoc(), TII->get(MI.getOpcode()))
+          .addReg(Z80::IX)
+          .addImm(address + offset);
+        MI.eraseFromParent();
+        continue;
+      }
     }
   }
 
