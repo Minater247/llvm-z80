ee83b6d515069394973e70088f64013336daf957
Indrek Mandre <indrek@mare.ee>
2025-02-23 23:46:39 +0200

    skip download check if binutils present

--- install.sh
+++ install.sh
@@ -8,24 +8,26 @@ cd build
 # This marks where we install the software
 PREFIX=/opt/local/z80-none-elf
 
-curl -C - -LO https://ftp.gnu.org/gnu/binutils/binutils-2.43.tar.lz
+if [ ! -f binutils-2.43/configure ]; then
+  curl -C - -LO https://ftp.gnu.org/gnu/binutils/binutils-2.43.tar.lz
+  tar xf binutils-2.43.tar.lz
+fi
 
-(tar xf binutils-2.43.tar.lz;
-cd binutils-2.43 \
+(cd binutils-2.43 \
     && ./configure --target=z80-none-elf --program-prefix=z80-none-elf- --prefix=$PREFIX \
     && make -j$(nproc) \
     && make install
 )
 
-#(cd $PREFIX/bin; for i in ez80-none-*; do zname=z80-$(echo $i|cut -f2- -d"-"); if [ ! -f $zname ]; then ln -s $i $zname; fi; done)
-
-cmake -G Ninja -DLLVM_ENABLE_ASSERTIONS=ON -DLLVM_ENABLE_PROJECTS="clang" \
-                      -DCMAKE_INSTALL_PREFIX=$PREFIX \
-                      -DCMAKE_BUILD_TYPE=Release \
-                      -DLLVM_EXPERIMENTAL_TARGETS_TO_BUILD=Z80 \
-                      -DLLVM_TARGETS_TO_BUILD= \
-                      -DLLVM_DEFAULT_TARGET_TRIPLE=z80-none-elf \
-                      ../llvm
+if [ ! -f build.ninja ]; then
+  cmake -G Ninja -DLLVM_ENABLE_ASSERTIONS=ON -DLLVM_ENABLE_PROJECTS="clang" \
+                        -DCMAKE_INSTALL_PREFIX=$PREFIX \
+                        -DCMAKE_BUILD_TYPE=Release \
+                        -DLLVM_EXPERIMENTAL_TARGETS_TO_BUILD=Z80 \
+                        -DLLVM_TARGETS_TO_BUILD= \
+                        -DLLVM_DEFAULT_TARGET_TRIPLE=z80-none-elf \
+                        ../llvm
+fi
 
 ninja install
 
