1f61582e30bfaf2fa8c4001d4eec4ad39897c657
Indrek Mandre <indrek@mare.ee>
2025-02-24 22:31:52 +0200

    also support the Kempston joystick

--- z80/samples/hello_keyboard/main.cpp
+++ z80/samples/hello_keyboard/main.cpp
@@ -4,25 +4,27 @@
 uint8_t read_keypress_mask() __attribute__((noinline));
 uint8_t read_keypress_mask() {
     return ZX::Keyboard::pressed_mask(
-        ZX::Keyboard::KEY_LEFT, 0,
-        ZX::Keyboard::KEY_O, 0,
-        ZX::Keyboard::KEY_RIGHT, 1,
-        ZX::Keyboard::KEY_P, 1,
-        ZX::Keyboard::KEY_UP, 2,
-        ZX::Keyboard::KEY_Q, 2,
-        ZX::Keyboard::KEY_DOWN, 3,
-        ZX::Keyboard::KEY_A, 3,
+        ZX::Keyboard::KEY_RIGHT, 0,
+        ZX::Keyboard::KEY_P, 0,
+        ZX::Keyboard::KEY_LEFT, 1,
+        ZX::Keyboard::KEY_O, 1,
+        ZX::Keyboard::KEY_DOWN, 2,
+        ZX::Keyboard::KEY_A, 2,
+        ZX::Keyboard::KEY_UP, 3,
+        ZX::Keyboard::KEY_Q, 3,
         ZX::Keyboard::KEY_SPACE, 4
     );
 }
 
 int main()
 {
-  ZX::Console::at(4, 19);
+  ZX::Console::at(2, 18);
   ZX::Console::print("Use arrow keys or OPQA.");
-  ZX::Console::at(4, 20);
-  ZX::Console::print("Space to clear.");
-  ZX::Console::at(4, 21);
+  ZX::Console::at(2, 19);
+  ZX::Console::print("Or use the Kempston joystick.");
+  ZX::Console::at(2, 20);
+  ZX::Console::print("Space or FIRE to clear.");
+  ZX::Console::at(2, 21);
   ZX::Console::print("Press S to START!");
 
   while (!ZX::Keyboard::KEY_S.is_pressed())
@@ -33,24 +35,26 @@ int main()
 
   while (true) {
       uint8_t mask = 0;
-      for (int i = 0 ; i < 50; ++i)
+      for (int i = 0 ; i < 50; ++i) {
           mask |= read_keypress_mask();
+          mask |= ZX::Kempston::read();
+      }
 
       if (mask & (1 << 0))
-          --x;
-      if (mask & (1 << 1))
           ++x;
+      if (mask & (1 << 1))
+          --x;
       if (mask & (1 << 2)) {
-          if (y == 0)
-              y = 191;
-          else
-              --y;
-      }
-      if (mask & (1 << 3)) {
           if (y == 191)
               y = 0;
           else
               ++y;
+      }
+      if (mask & (1 << 3)) {
+          if (y == 0)
+              y = 191;
+          else
+              --y;
       }
 
       if (mask & (1 << 4))
--- z80/samples/hello_keyboard/zx.h
+++ z80/samples/hello_keyboard/zx.h
@@ -171,6 +171,20 @@ namespace ZX {
         }
     };
 
+    struct Kempston {
+        static uint8_t read() {
+            uint8_t a;
+            // Kempston Joystick is at port 0x001f
+            __asm__ volatile (
+                "in\ta, (0x1f)"
+                : "=a"(a)
+                : "a"((uint8_t)0)
+                :
+            );
+            return a;
+        }
+    };
+
     using Screen = _Screen<0x4000>;
 };
 
