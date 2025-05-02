7b1ec78c9bab013b96b755739329dc07ea2e28e8
Indrek Mandre <indrek@mare.ee>
2025-05-02 13:47:16 +0300

    z80: describe all keys at ZX::Keyboard

--- z80/include/keyboard.h
+++ z80/include/keyboard.h
@@ -69,21 +69,54 @@ namespace ZX {
             }
         };
 
-        static Key<'_', 0xfe, 0> KEY_SHIFT;
-        static Key<'p', 0xdf, 0> KEY_P;
-        static Key<'o', 0xdf, 1> KEY_O;
-        static Key<'q', 0xfb, 0> KEY_Q;
-        static Key<'a', 0xfd, 0> KEY_A;
-        static Key<'s', 0xfd, 1> KEY_S;
-        static Key<'k', 0xbf, 2> KEY_K;
-        static Key<'m', 0x7f, 2> KEY_M;
-        static Key<'b', 0x7f, 4> KEY_B;
-        static Key<' ', 0x7f, 0> KEY_SPACE;
+        static Key<'1', 0xf7, 0> KEY_1;
+        static Key<'2', 0xf7, 1> KEY_2;
+        static Key<'3', 0xf7, 2> KEY_3;
+        static Key<'4', 0xf7, 3> KEY_4;
         static Key<'5', 0xf7, 4> KEY_5;
+
         static Key<'6', 0xef, 4> KEY_6;
         static Key<'7', 0xef, 3> KEY_7;
         static Key<'8', 0xef, 2> KEY_8;
         static Key<'9', 0xef, 1> KEY_9;
+        static Key<'0', 0xef, 0> KEY_0;
+
+        static Key<'q', 0xfb, 0> KEY_Q;
+        static Key<'w', 0xfb, 1> KEY_W;
+        static Key<'e', 0xfb, 2> KEY_E;
+        static Key<'r', 0xfb, 3> KEY_R;
+        static Key<'t', 0xfb, 4> KEY_T;
+
+        static Key<'y', 0xdf, 4> KEY_Y;
+        static Key<'u', 0xdf, 3> KEY_U;
+        static Key<'i', 0xdf, 2> KEY_I;
+        static Key<'o', 0xdf, 1> KEY_O;
+        static Key<'p', 0xdf, 0> KEY_P;
+
+        static Key<'a', 0xfd, 0> KEY_A;
+        static Key<'s', 0xfd, 1> KEY_S;
+        static Key<'d', 0xfd, 2> KEY_D;
+        static Key<'f', 0xfd, 3> KEY_F;
+        static Key<'g', 0xfd, 4> KEY_G;
+
+        static Key<'h', 0xbf, 4> KEY_H;
+        static Key<'j', 0xbf, 3> KEY_J;
+        static Key<'k', 0xbf, 2> KEY_K;
+        static Key<'l', 0xbf, 1> KEY_L;
+        static Key<' ', 0xbf, 0> KEY_ENTER;
+
+        static Key<'_', 0xfe, 0> KEY_SHIFT;
+        static Key<'z', 0xfe, 1> KEY_Z;
+        static Key<'x', 0xfe, 2> KEY_X;
+        static Key<'c', 0xfe, 3> KEY_C;
+        static Key<'v', 0xfe, 4> KEY_V;
+
+        static Key<'b', 0x7f, 4> KEY_B;
+        static Key<'n', 0x7f, 3> KEY_N;
+        static Key<'m', 0x7f, 2> KEY_M;
+        static Key<' ', 0x7f, 1> KEY_SYM;
+        static Key<' ', 0x7f, 0> KEY_SPACE;
+
         static Key<' ', 0xef, 2, true> KEY_RIGHT;
         static Key<' ', 0xef, 3, true> KEY_UP;
         static Key<' ', 0xef, 4, true> KEY_DOWN;
